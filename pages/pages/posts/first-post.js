76f0ec9e56a728f1ca440538ca5050bf50b348fa
Mounir Nessim <mnessim@ad.wcpss.net>
2023-06-05 09:04:59 -0400

    First blog post

--- pages/pages/posts/first-post.js
+++ pages/pages/posts/first-post.js
@@ -1,10 +1,31 @@
 export default function FirstPost() {
     return(
-        <div className="container">
-            <h2></h2>
-            <h4>Date: 6/4/2023</h4>
+        <div className="container blog">
+            <h2>Teaching Year in Review 2022-2023</h2>
+            <h4>June 5, 2023</h4>
             <p>
-
+                As the 2022-2023 school year draws to a close, I find it appropriate to take some time to look back over the year and organize some thoughts. This has been my second year as a K-12 classroom teacher. Last year, I taught high school in a general music role. This year, I landed at a middle school teaching orchestra. The difference between the two years has been tremendous. I am in a different district with a very different demographic makeup. I am teaching what I was trained to teach, instead of cobbling together a curriculum more or less from scratch. Although I thought I would prefer older students, I find the middle school kids to be far kinder and easier to work with. 
+            </p>
+            <p>
+                One other tremendous advantage of my current role is that I share an office with our school's band director. We were both new to the school this year, and I was a little unsure of how we would get along. Fortunately, we get along quite well. More than that, she has been a great mentor to me over the course of the year. While this is my second year teaching, it is her 15th. She has been at many schools in many districts, which gives me a great sense of perspective. While she is not technically my mentor, she is always the first person I go to with questions or concerns. Often times, I'm not even seeking advice per se. I may just be venting to the closest coworker. I would like to think that this has been a two-way street to some extent. The two of us find ourselves very isolated from the rest of the faculty, due in part to our physical location in the building. We also naturally serve supporting roles for each other with things like concerts and extra-curricular activities. 
+            </p>
+            <p>
+                Speaking of, this year has involved far more work outside of work hours than last year. The last few weeks especially have been full of after school activities. From concerts to sports duties to meetings, it is extremely rare that I work a 40 hour week. I keep saying to myself that I should track my hours for a week to see, but somehow I've never found the time. Certain events have even forced me to travel out of town and spend my whole Saturday "volunteering." This sort of thing is simply par for the course for performing arts teachers. It is somewhat offset by a modest pay supplement. Despite the supplement, one of my biggest complaints about the job is how much of my time it takes up. 
+            </p>
+            <p>
+                Another one of the big complaints I have about teaching is how utterly draining it can be. I wake up for work at 4:30ish every day. I try to be in the car by 6:10 so I can be at my desk around 6:30. While our school day doesn't begin until 7:30, we need to be in our rooms no later than 7:00 am, because that is when students begin to arrive. Bus driver shortages mean that some busses double up on routes, dropping off their first group of students before 7:00 am usually. 
+            </p>
+            <p>
+                Anyone with eyes and ears in the US has probably heard about staff shortages at schools. It has had a significant impact on my school this year. We lost a few teachers throughout the course of the year for various reasons. As far as I know, only one teacher was actually replaced. There has also been a massive shortage of subs. This means that we have frequently had to give up our planning period to cover another teacher's class, either because that teacher was no longer employed at the school, or simply because they were out sick.
+            </p>
+            <p>
+                While I could go over many more specific examples of difficulties at work, it will save a considerable amount of time simply to sum it up as a lack of adequate time and resources to complete all the work that needs to be done. Whether it's staff shortages, lack of planning time, lack of funding, or some other issue, we are always asked to do more with less. I can't blame my administrators, because most of these issues are not their fault. These are systemic issues with public education in North Carolina, and in the entire country. Most people are also aware of the vicious cycle that education is currently in. The job is excessively difficult, which leads people to leave. When people leave, however, the workload for those who remain increases. This makes the job more difficult, perpetuating the cycle. 
+            </p>
+            <p>
+                Teaching is an undeniably unattractive profession. The hours are long, the responsibilities and liabilities are endless, the pay is low, the future prospects are few, and there is little to no prestige associated with the job. Teaching as it stands in the US today survives only as a labor of love. Those who teach and continue to teach do it because they believe in the mission of providing a free, high-quality public education to our nation's youth. Furthermore, they believe that they can have a positive impact on the kids that they come in contact with on the job. 
+            </p>
+            <p>
+                I do believe in the mission of education, but I am also wary of a profession that takes far more from me than it gives.
             </p>
         </div>
     )
